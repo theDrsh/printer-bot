925ab2fdf5bd65cadab9767be53b155e93a9d76e
Daniel Rush <rush.daniel95@gmail.com>
2020-12-19 17:28:44 -0800

    gitignore, trying to get this to work on remote

--- bot.py
+++ bot.py
@@ -4,15 +4,6 @@ import time
 import yaml
 
 
-
-class DiscordBot(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        if message.channel.name in secrets.allowed_channels:
-            print('Message from {0.author}: {0.content}'.format(message))
-    
 class OctoprintEvents():
     def __init__(self):
         self.event_log_path = secrets.event_log
@@ -23,16 +14,21 @@ class OctoprintEvents():
         with open(self.event_log_path, "w") as event_log:
             event_log.write("")
 
-
 class PrinterBot():
     def __init__(self):
-        self.bot = DiscordBot()
-        self.bot.run(secrets.bot_token)
         self.events = OctoprintEvents()
-    
+
+class DiscordBot(discord.Client):
+    async def on_ready(self):
+        pb = PrinterBot()
+        while(1):
+            time.sleep(1)
+            pb.events.update()
+
+    async def on_message(self, message):
+        if message.channel.name in secrets.allowed_channels:
+            print('Message from {0.author}: {0.content}'.format(message))
 
 if __name__ == "__main__":
-    pbot = PrinterBot()
-    while(1):
-        pbot.events.update()
-        time.sleep(1)
+    bot = DiscordBot()
+    bot.run(secrets.bot_token)
